787fee0ba7f41189fdb7d2ce671242bcd9a6f69a
Matej Skubic <839965+matejskubic@users.noreply.github.com>
2025-03-19 22:42:19 +0100

    Add project files.

---
+++ NormalizeXml/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: NormalizeXml <filename>");
+    return;
+}
+
+XmlDocument xml = new();
+xml.Load(args[0]);
+xml.Save(args[0]);
